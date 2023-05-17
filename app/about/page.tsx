2441c8c4b57041c92fd407f67e86a9499511c96e
Narges <nargeskhatami.h@gmail.com>
2023-05-18 01:37:52 +0330

    change style of about in mobile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="w-3/5 text-white bg-white/10 rounded-xl p-12">
+    <div className="w-4/5 lg:w-3/5 text-white bg-white/10 rounded-xl p-6 lg:p-12">
       <h2 className="w-fit mx-auto text-2xl mb-6 font-semibold">About</h2>
       <p className="text-justify mx-auto leading-relaxed">
         Welcome to NextWeather, a simple and elegant weather app built with
